5468b95c366201606e4d0360b1fc1106f49af0cb
Reza <seda1390@gmail.com>
2022-07-12 23:55:36 -0700

    Update Amzon_OA_Leetcode_937_Reorder Data in Log Files.py

--- Amzon_OA_Leetcode_937_Reorder Data in Log Files.py
+++ Amzon_OA_Leetcode_937_Reorder Data in Log Files.py
@@ -24,18 +24,24 @@ Example 2:
 
 
 from typing import List
+    
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters = []
+        nums = []
+        
+        for log in logs:
+            ident, cont = log.split(' ', 1)
+            (letters if cont[0].isalpha() else nums).append((cont, ident))
+        letters.sort()
+        
+        return [f'{i} {c}' for c, i in letters + nums]    
+      
+      
+      
 
-def reorder_log_files(logs: List[str]) -> List[str]:
-    alphas = []
-    nums = []
-    for log in logs:
-        ident, cont = log.split(' ', 1)
-        (alphas if cont[0].isalpha() else nums).append((cont, ident))
-    alphas.sort()
-    return [f'{i} {c}' for c, i in alphas + nums]
-
-if __name__ == '__main__':
-    logs = [input() for _ in range(int(input()))]
-    res = reorder_log_files(logs)
-    for line in res:
-        print(line)
+# if __name__ == '__main__':
+#     logs = [input() for _ in range(int(input()))]
+#     res = reorder_log_files(logs)
+#     for line in res:
+#         print(line)
